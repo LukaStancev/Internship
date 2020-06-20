0b22ec444669cabf7bfdda97bbe39436e24409ba
Vivian SALINO <firstname.surname@irsn.fr>
2020-06-20 02:31:57 +0200

    Verified update to DRAGON5 rev1879 for fuel
    Works flawlessly !

--- Drakkar/Fuel.sh
+++ Drakkar/Fuel.sh
@@ -6,43 +6,45 @@
 rm -rf Fuel/Linux_x86_64 Fuel/save Fuel/kinf Fuel/Saphyb
 cd Fuel/data
 
+Version5=v5bev1879
+
 # EvoNominale
 sed -i 's/STRING Save := "TIH_UOX_.*/STRING Save := "TIH_UOX_255" ;/' EvoNominale.x2m
 sed -i 's/INTEGER Teneur_I.*/INTEGER Teneur_I := 255    ;/' EvoNominale.x2m
 sed -i 's/STRING  ConfigPyrex.*/STRING  ConfigPyrex := "Py8" ;/' EvoNominale.x2m
-srun dragon EvoNominale.x2m v5bev1487 &
+srun dragon EvoNominale.x2m $Version5 &
 sleep 10
 sed -i 's/STRING Save := "TIH_UOX_.*/STRING Save := "TIH_UOX_255" ;/' EvoNominale.x2m
 sed -i 's/INTEGER Teneur_I.*/INTEGER Teneur_I := 255    ;/' EvoNominale.x2m
 sed -i 's/STRING  ConfigPyrex.*/STRING  ConfigPyrex := "Py12" ;/' EvoNominale.x2m
-srun dragon EvoNominale.x2m v5bev1487 &
+srun dragon EvoNominale.x2m $Version5 &
 sleep 10
 sed -i 's/STRING Save := "TIH_UOX_.*/STRING Save := "TIH_UOX_195" ;/' EvoNominale.x2m
 sed -i 's/INTEGER Teneur_I.*/INTEGER Teneur_I := 195    ;/' EvoNominale.x2m
 sed -i 's/STRING  ConfigPyrex.*/STRING  ConfigPyrex := "None" ;/' EvoNominale.x2m
-srun dragon EvoNominale.x2m v5bev1487 &
+srun dragon EvoNominale.x2m $Version5 &
 sleep 10
 sed -i 's/STRING Save := "TIH_UOX_.*/STRING Save := "TIH_UOX_310" ;/' EvoNominale.x2m
 sed -i 's/INTEGER Teneur_I.*/INTEGER Teneur_I := 310    ;/' EvoNominale.x2m
 sed -i 's/STRING  ConfigPyrex.*/STRING  ConfigPyrex := "None" ;/' EvoNominale.x2m
-srun dragon EvoNominale.x2m v5bev1487 &
+srun dragon EvoNominale.x2m $Version5 &
 sleep 10
 sed -i 's/STRING Save := "TIH_UOX_.*/STRING Save := "TIH_UOX_310" ;/' EvoNominale.x2m
 sed -i 's/INTEGER Teneur_I.*/INTEGER Teneur_I := 310    ;/' EvoNominale.x2m
 sed -i 's/STRING  ConfigPyrex.*/STRING  ConfigPyrex := "Py12" ;/' EvoNominale.x2m
-srun dragon EvoNominale.x2m v5bev1487
+srun dragon EvoNominale.x2m $Version5
 # MakeBib1BU
 sed -i 's/EvoName=.*/EvoName="TIH_UOX_195"/' MakeBib1BU.access
-srun dragon MakeBib1BU.x2m v5bev1487
+srun dragon MakeBib1BU.x2m $Version5
 sleep 10
 sed -i 's/EvoName=.*/EvoName="TIH_UOX_255-Py8"/' MakeBib1BU.access
-srun dragon MakeBib1BU.x2m v5bev1487 &
+srun dragon MakeBib1BU.x2m $Version5 &
 sleep 10
 sed -i 's/EvoName=.*/EvoName="TIH_UOX_255-Py12"/' MakeBib1BU.access
-srun dragon MakeBib1BU.x2m v5bev1487 &
+srun dragon MakeBib1BU.x2m $Version5 &
 sleep 10
 sed -i 's/EvoName=.*/EvoName="TIH_UOX_310-Py12"/' MakeBib1BU.access
-srun dragon MakeBib1BU.x2m v5bev1487 &
+srun dragon MakeBib1BU.x2m $Version5 &
 sleep 10
 sed -i 's/EvoName=.*/EvoName="TIH_UOX_310"/' MakeBib1BU.access
-srun dragon MakeBib1BU.x2m v5bev1487
+srun dragon MakeBib1BU.x2m $Version5
