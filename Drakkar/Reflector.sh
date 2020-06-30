90d06e34f0ad309881aa6c7d5a142cc322a50825
Vivian SALINO <firstname.surname@irsn.fr>
2020-06-30 16:52:53 +0200

    Clean up after issue #2 completion
    Separate fuel, reflector and full core modelling are no longer
    necessary.

--- Drakkar/Reflector.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -c 1  ## Number of cores to be reserved
-#SBATCH -t 0-06:00:00
-#SBATCH --mem-per-cpu=4G
-
-rm -rf Reflector/Linux_x86_64 Reflector/save/*
-cd Reflector/data
-
-srun dragon TousPaliers.x2m v5bev1879
