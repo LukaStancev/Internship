701194301146904738bd230e8fc7fa7c102a8301
Vivian SALINO <firstname.surname@irsn.fr>
2021-07-05 11:15:31 +0200

    Fix node selection based on Serpent memory need
    Fixed values were used instead of the desired variables.

--- Serpent/Exclude.sh
+++ Serpent/Exclude.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# This script produces the list of (un)suitable Farux nodes for a specific problem (n CPUs, mem Gb of memory)
+# This script produces the list of (un)suitable Farux nodes for a specific problem that requires "n" CPUs and "mem" Gb of memory
 # These actions are necessary to overcome the problems of large heterogeneity of this cluster
 # Excluded : (Total node memory [Mb]) - truncate((Number of CPUs)/(n CPUs))*(mem Gb)*(1024 Mb/Gb) < 0
 # Included : (Total node memory [Mb]) - truncate((Number of CPUs)/(n CPUs))*(mem Gb)*(1024 Mb/Gb) > 0
 # Usage : ./Exclude.sh
-n=5 # CPUs
-mem=30 # Gb of memory required
-sinfo -Nel | tail -n +3 | awk '{if ($7-int($5/3)*20*1024<0) print $1}' | sort -n > Exclude.txt
-sinfo -Nel | tail -n +3 | awk '{if ($7-int($5/3)*20*1024>0) print $1}' | sort -n > Include.txt
+n=3 # CPUs
+mem=20 # Gb of memory required
+sinfo -Nel | tail -n +3 | awk -v n="$n" -v mem="$mem" '{if ($7-int($5/n)*mem*1024<0) print $1}' | sort -n > Exclude.txt
+sinfo -Nel | tail -n +3 | awk -v n="$n" -v mem="$mem" '{if ($7-int($5/n)*mem*1024>0) print $1}' | sort -n > Include.txt
