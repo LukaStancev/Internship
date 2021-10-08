3d80790160c891dd5fe04b0acc5a5da3eeb6a760
Vivian SALINO <firstname.surname@irsn.fr>
2021-10-08 07:56:11 +0200

    Add CASMO-5 output: detector fluxes/cross sections
    Also, add CASMO-5 models for each individual assembly.

--- CASMO-5/Launch.sh
+++ CASMO-5/Launch.sh
@@ -2,7 +2,7 @@
 
 . ~/.profile.perso
 
-rm -f *.out *.log
+rm -f FSH2.out FSH2.log
 
 # Use JEFF-3.1.1 data and 12 cores
 sbatch --mem=12000 -c 12 --wrap="/soft_snc/scripts/cas5 FSH2.inp -S 4G -N /soft_snc/CASMO5/library/j311.200.586.bin"
---
+++ CASMO-5/LaunchPy16.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+. ~/.profile.perso
+
+rm -f Py16.out Py16.log
+
+# Use JEFF-3.1.1 data and 12 cores
+sbatch --mem=12000 -c 12 --wrap="/soft_snc/scripts/cas5 Py16.inp -S 4G -N /soft_snc/CASMO5/library/j311.200.586.bin"
+
+# TIF generation uses much more memory
+#sbatch --mem=250000 -c 12 --wrap="/soft_snc/scripts/cas5 Py16.inp -S 250G -N /soft_snc/CASMO5/library/j311.200.586.bin"
